e62bd5dcf950b4d63b1224676c84f6bc5c5da734
jackhcarpenter <carpenterj3@cardiff.ac.uk>
2023-07-11 17:37:59 +0100

    Update 1-mapping_bowtie2.sh
    First upload of mapping script (unfinished)

---
+++ 1-mapping_bowtie2.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#SBATCH --partition=QUEUE_NAME       # the requested queue
+#SBATCH --nodes=1              # number of nodes to use
+#SBATCH --tasks-per-node=1     # for parallel distributed jobs
+#SBATCH --cpus-per-task=8      # for multi-threaded jobs
+#SBATCH --mem-per-cpu=4G      # in megabytes, unless unit explicitly stated
+#SBATCH --error=logs/%J.err         # redirect stderr to this file
+#SBATCH --output=logs/%J.out        # redirect stdout to this file
+#SBATCH --mail-user=USERNAME@INSTITUTIONAL_ADDRESS      # email
+#SBATCH --mail-type=BEGIN,END,FAIL      # email on job start, end, and/or failure
+
+#################################################################################
+# Print Slurm Parameters to Console
+#################################################################################
+
+echo "Usable Environment Variables:"
+echo "============================="
+echo "hostname=$(hostname)"
+echo \$SLURM_JOB_ID=${SLURM_JOB_ID}
+echo \$SLURM_NTASKS=${SLURM_NTASKS}
+echo \$SLURM_NTASKS_PER_NODE=${SLURM_NTASKS_PER_NODE}
+echo \$SLURM_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
+echo \$SLURM_JOB_CPUS_PER_NODE=${SLURM_JOB_CPUS_PER_NODE}
+echo \$SLURM_MEM_PER_CPU=${SLURM_MEM_PER_CPU}
+
+#################################################################################
+# Modulels to Load and Setup
+#################################################################################
+
+module load bowtie2/v2.4.1
+module load samtools/1.10
+module load bamtools/v2.5.1
+
+## point to the directory containing the reference genome where sequences will be mapped
+export refdir=/your/reference/directory
+
+## point to the working directory
+export workingdir=/your/working/dir
+
+#################################################################################
+# Main CMD
+#################################################################################
+
+## Index the genome for quicker access by bowtie2 during alignment
+bowtie2-build $refdir/SPECIES_GENOME.dna.toplevel.fa $refdir/SPECIES_GENOME.INDEX.fa
