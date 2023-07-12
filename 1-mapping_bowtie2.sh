b9c0b314b06861e158617c53a8f4637664a6a998
jackhcarpenter <carpenterj3@cardiff.ac.uk>
2023-07-12 11:39:28 +0100

    Update 1-mapping_bowtie2.sh
    finished first version of mapping code

--- 1-mapping_bowtie2.sh
+++ 1-mapping_bowtie2.sh
@@ -32,15 +32,66 @@ module load bowtie2/v2.4.1
 module load samtools/1.10
 module load bamtools/v2.5.1
 
-## point to the directory containing the reference genome where sequences will be mapped
+## point to the directory containing the reference genome where sequences will be
+## mapped
 export refdir=/your/reference/directory
 
 ## point to the working directory
 export workingdir=/your/working/dir
+
+##REMEMBER: set up any directories that the software needs in this script in case 
+##it is unable to do so itself
 
 #################################################################################
 # Main CMD
 #################################################################################
 
 ## Index the genome for quicker access by bowtie2 during alignment
-bowtie2-build $refdir/SPECIES_GENOME.dna.toplevel.fa $refdir/SPECIES_GENOME.INDEX.fa
+bowtie2-build \
+    $refdir/SPECIES_GENOME.dna.toplevel.fa\
+    $refdir/SPECIES_GENOME.INDEX.fa
+
+## List of sequences to map to indexed reference genome
+list=(
+    "sample_input1" "sample_inputn" "sample_ip1" "sample_ipn" "sample_neg1"\ 
+    "sample_negn" "control_input1" "control_inputn" "control_ip1" "control_ipn"\ 
+    "control_neg1" "control_negn")
+
+## Map forward and reverse reads to the indexed referenced genome
+for i in ${list[@]}
+do
+        
+        echo ${i}
+
+        ## Align the sequences to the genome
+        bowtie2 \
+        --maxins 500 \
+        --fr \
+        -p 8 \
+        -x $refdir/SPECIES.GENOME.INDEX.fa \
+        -1 $workingdir/trimmed_reads${i}_fp1.fastq.gz \
+        -2 $workingdir/trimmed/${i}_fp2.fastq.gz \
+        -S $workingdir/bowtie/${i}.sam
+
+        ## Compress the aligned sam files to bam files
+        samtools view \
+        -bS $workingdir/bowtie/${i}.sam \
+        > $workingdir/bowtie/${i}.bam \
+
+        ## Organise mapped reads and index them for faster access during 
+        ## downstream processing
+        samtools view \
+        -@ ${SLURM_CPUS_PER_TASK} \
+        -o $workingdir/bowtie/${i}.sorted.bam \
+        $workingdir/bowtie/${i}.bam
+
+        samtools index \
+        $workingdir/${i}.sorted.bam
+        
+        ## Run some stats on aligned/mapped reads
+        bamtools stats \
+        -in $workingdir/bowtie/${i}.sorted.bam \
+        > $workingdir/${i}.sorted.stats.txt
+        ## look at the stats files to quality check the data
+
+done
