37bf8679e685c422e0a8de79c7b4c117f8d130e6
jackhcarpenter <carpenterj3@cardiff.ac.uk>
2023-07-12 16:51:55 +0100

    Errors and uploads
    First upload of 2-markduplicates.sh and 3-wigfiles.sh. Also fixed a bug in 1-mapping_bowtie2.sh that was causing an error on line 89 due to wrong file path

---
+++ 2-markduplicates.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#SBATCH --partition=QUEUE_NAME       # the requested queue
+#SBATCH --nodes=1              # number of nodes to use
+#SBATCH --tasks-per-node=1     #
+#SBATCH --cpus-per-task=4      #  
+#SBATCH --mem-per-cpu=8G       # in megabytes, unless unit explicitly stated
+#SBATCH --time=6:0:0
+#SBATCH --error=logs/%J.err         # redirect stderr to this file
+#SBATCH --output=logs/%J.out        # redirect stdout to this file
+#SBATCH --mail-user=USERNAME@INSTITUTIONAL_ADDRESS # email address used for event notification
+#SBATCH --mail-type=BEGIN,END,FAIL # email on job start, end, and/or failure
+
+#################################################################################
+# Print Slurm Parameters to Console
+#################################################################################
+
+echo "Usable Environment Variables:"
+echo "============================="
+echo "hostname=$(hostname)"
+echo \$SLURM_JOB_ID=${SLURM_JOB_ID}
+echo \$SLURM_NTASKS=${SLURM_NTASKS}
+echo \$SLURM_NTASKS_PER_NODE=${SLURM_NTASKS_PER_NODE}
+echo \$SLURM_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
+echo \$SLURM_JOB_CPUS_PER_NODE=${SLURM_JOB_CPUS_PER_NODE}
+echo \$SLURM_MEM_PER_CPU=${SLURM_MEM_PER_CPU}
+
+#################################################################################
+# Modulels to Load and Setup
+#################################################################################
+
+module load picard/2.22.2
+module load bamtools/v2.5.1
+
+## point to the working directory
+export workingdir=/your/working/dir
+
+##REMEMBER: set up any directories that the software needs in this script in case 
+##it is unable to do so itself
+
+#################################################################################
+# Main CMD
+#################################################################################
+
+list=(
+        "sample_input1" "sample_inputn" "sample_ip1" "sample_ipn" "sample_neg1"\
+        "sample_negn" "control_input1" "control_inputn" "control_ip1" "control_ipn"\
+        "control_neg1" "control_negn")
+
+for i in ${list[@]}
+
+do
+        echo ${i}
+
+## Mark the duplicated reads
+        java \
+        -jar $PICARD MarkDuplicates \
+        I=$workingdir/bowtie/${i}.sorted.bam \
+        O=$workingdir/markdup/${i}.markdup.bam \
+        M=$workingdir/markdup/${i}.metrics.markdup.txt \
+        REMOVE_DUPLICATES=false \
+        VALIDATION_STRINGENCY=SILENT
+
+        bamtools stats \
+        -in $workingdir/markdup/${i}.markdup.bam \
+        > $workingdir/markdup/${i}.markdup.dupstats.txt
+
+## Remove the duplicated reads
+        java \
+        -jar $PICARD MarkDuplicates \
+        I=$workingdir/bowtie/${i}.sorted.bam \
+        O=$workingdir/markdup/${i}.rmdup.bam \
+        M=$workingdir/markdup/${i}.metrics.rmdup.txt \
+        REMOVE_DUPLICATES=true \
+        VALIDATION_STRINGENCY=SILENT
+
+        bamtools stats \
+        -in $workingdir/markdup/${i}.rmdup.bam \
+        > $workingdir/markdup/${i}.rmdup.dupstats.txt
+
+## Now look at the files to see if it is better to keep or remove duplicates
+done
--- 3-wigfiles.sh
+++ 3-wigfiles.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+#!/bin/bash
 #SBATCH --partition=QUEUE_NAME       # the requested queue
 #SBATCH --nodes=1              # number of nodes to use
 #SBATCH --tasks-per-node=1     # for parallel distributed jobs
 #SBATCH --cpus-per-task=8      # for multi-threaded jobs
-#SBATCH --mem-per-cpu=4G      # in megabytes, unless unit explicitly stated
+#SBATCH --mem-per-cpu=2G      # in megabytes, unless unit explicitly stated
+#SBATCH --time=20:00:00
 #SBATCH --error=logs/%J.err         # redirect stderr to this file
 #SBATCH --output=logs/%J.out        # redirect stdout to this file
-#SBATCH --mail-user=USERNAME@INSTITUTIONAL_ADDRESS      # email
-#SBATCH --mail-type=BEGIN,END,FAIL      # email on job start, end, and/or failure
+#SBATCH --mail-user=USERNAME@INSTITUTIONAL_ADDRESS # email address used for event notification
+#SBATCH --mail-type=BEGIN,END,FAIL # email on job start, end, and/or failure
 
 #################################################################################
 # Print Slurm Parameters to Console
@@ -28,9 +30,9 @@ echo \$SLURM_MEM_PER_CPU=${SLURM_MEM_PER_CPU}
 # Modulels to Load and Setup
 #################################################################################
 
-module load bowtie2/v2.4.1
 module load samtools/1.10
-module load bamtools/v2.5.1
+module load bedtools/2.29.1
+module load deeptools/3.5.1
 
 ## point to the directory containing the reference genome where sequences will be
 ## mapped
@@ -46,52 +48,27 @@ export workingdir=/your/working/dir
 # Main CMD
 #################################################################################
 
-## Index the genome for quicker access by bowtie2 during alignment
-bowtie2-build \
-    $refdir/SPECIES_GENOME.dna.toplevel.fa\
-    $refdir/SPECIES_GENOME.INDEX.fa
 
-## List of sequences to map to indexed reference genome
-list=(
-        "sample_input1" "sample_inputn" "sample_ip1" "sample_ipn" "sample_neg1"\
+list=("sample_input1" "sample_inputn" "sample_ip1" "sample_ipn" "sample_neg1"\
         "sample_negn" "control_input1" "control_inputn" "control_ip1" "control_ipn"\
         "control_neg1" "control_negn")
 
-## Map forward and reverse reads to the indexed referenced genome
 for i in ${list[@]}
 do
-        
         echo ${i}
 
-        ## Align the sequences to the genome
-        bowtie2 \
-        --maxins 500 \
-        --fr \
-        -p 8 \
-        -x $refdir/SPECIES.GENOME.INDEX.fa \
-        -1 $workingdir/trimmed_reads${i}_fp1.fastq.gz \
-        -2 $workingdir/trimmed_reads/${i}_fp2.fastq.gz \
-        -S $workingdir/bowtie/${i}.sam
+## Use samtools to create an index in fasta format so as betools can access quickly
+    samtools faidx $refdir/SPECIES.GENOME.INDEX.fa
 
-        ## Compress the aligned sam files to bam files
-        samtools view \
-        -bS $workingdir/bowtie/${i}.sam \
-        > $workingdir/bowtie/${i}.bam \
+## Bin (make discrete) map sequences to create histograms 
+        bedtools genomecov \
+        -ibam $workingdir/bowtie/${i}.sorted.bam \
+        -bg \
+        -g $redir/SPECIES.GENOME.INDEX.fa.fai > $workingdir/beds/${i}.bedgraph
 
-        ## Organise mapped reads and index them for faster access during 
-        ## downstream processing
-        samtools view \
-        -@ ${SLURM_CPUS_PER_TASK} \
-        -o $workingdir/bowtie/${i}.sorted.bam \
-        $workingdir/bowtie/${i}.bam
-
-        samtools index \
-        $workingdir/bowtie/${i}.sorted.bam
-        
-        ## Run some stats on aligned/mapped reads
-        bamtools stats \
-        -in $workingdir/bowtie/${i}.sorted.bam \
-        > $workingdir/bowtie/${i}.sorted.stats.txt
-        ## look at the stats files to quality check the data
+## Generate a coverage track to view in Integrated Genome Viewer
+        bamCoverage \
+        -b $workingdir/bowtie/${i}.sorted.bam \
+        -o $workingdir/wigs/${i}.bw
 
 done
